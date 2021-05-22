9a8d557d3b573764e2c998a7a78ee6dceb90a43f
sirDudesALot <johnclivejonesuk@hotmail.co.uk>
2021-05-22 14:14:37 +0100

    Initial project setup

---
+++ Challenge389TheMontyHallProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge389TheMontyHallProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
