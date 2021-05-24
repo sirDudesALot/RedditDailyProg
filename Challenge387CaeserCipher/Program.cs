63ea7a3c679bb5cac65f1aa3fc5dd27b9afb0ba0
sirDudesALot <johnclivejonesuk@hotmail.co.uk>
2021-05-24 09:48:08 +0100

    Completed challenge 387 optional bonus 2

--- Challenge387CaeserCipher/Program.cs
+++ Challenge387CaeserCipher/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Challenge387CaeserCipher
@@ -7,6 +8,40 @@ namespace Challenge387CaeserCipher
     class Program
     {
         private const string _lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+
+        static List<Tuple<char, int>> GetLetterScoreList()
+        {
+            List<Tuple<char, int>> letterScoreList = new()
+            {
+                new('a', 3),
+                new('b', -1),
+                new('c', 1),
+                new('d', 1),
+                new('e', 4),
+                new('f', 0),
+                new('g', 0),
+                new('h', 2),
+                new('i', 2),
+                new('j', -5),
+                new('k', -2),
+                new('l', 1),
+                new('m', 0),
+                new('n', 2),
+                new('o', 3),
+                new('p', 0),
+                new('q', -6),
+                new('r', 2),
+                new('s', 2),
+                new('t', 3),
+                new('u', 1),
+                new('v', -1),
+                new('w', 0),
+                new('x', -5),
+                new('y', 0),
+                new('z', -7)
+            };
+            return letterScoreList;
+        }
 
         static void TestCaeserChar()
         {
@@ -18,7 +53,6 @@ namespace Challenge387CaeserCipher
             toTest.Add(new('a', 24));
             toTest.Add(new('A', 0));
 
-
             toTest.ForEach(t => Console.WriteLine($"f({ t.Item1 }, { t.Item2 }) => { CaeserChar(t.Item1, t.Item2) }"));
         }
 
@@ -26,13 +60,13 @@ namespace Challenge387CaeserCipher
         {
             int pos = _lowerCaseLetters.IndexOf(letter);
             string lettersToUse = _lowerCaseLetters;
-            if(pos == -1)
+            if (pos == -1)
             {
                 lettersToUse = _lowerCaseLetters.ToUpper();
                 pos = lettersToUse.IndexOf(letter);
             }
 
-            if(pos > -1)
+            if (pos > -1)
             {
                 pos = (pos + shift) % 26;
                 return lettersToUse[pos];
@@ -41,13 +75,13 @@ namespace Challenge387CaeserCipher
             {
                 return letter;
             }
-            
+
         }
 
         static string Caesar(string originalStr, int shift)
         {
             StringBuilder output = new();
-            foreach(var c in originalStr)
+            foreach (var c in originalStr)
             {
                 output.Append(CaeserChar(c, shift));
             }
@@ -66,7 +100,7 @@ namespace Challenge387CaeserCipher
             values.Add(new("%", 1, "%"));
 
             values.ForEach(t => Console.WriteLine($"f({ t.Item1 }, { t.Item2 }) => { Caesar(t.Item1, t.Item2) }. " +
-                $"{ Caesar(t.Item1, t.Item2)==t.Item3 }"));
+                $"{ Caesar(t.Item1, t.Item2) == t.Item3 }"));
         }
 
         static void OptionalBonusOne()
@@ -78,12 +112,76 @@ namespace Challenge387CaeserCipher
                 $"{ Caesar(t.Item1, t.Item2) == t.Item3 }"));
         }
 
+        static List<string> GenerateAllPossibleOrigText(string originalText)
+        {
+            List<string> results = new();
+            for (int ix = 0; ix < 26; ix++)
+            {
+                results.Add(Caesar(originalText, ix));
+            }
+            return results;
+        }
+
+        static int GetLetterScore(char letter)
+        {
+            int score = 0;
+            List<Tuple<char,int>> letterScoreList = GetLetterScoreList();
+            var results =
+                from lsr in letterScoreList
+                where lsr.Item1 == letter || lsr.Item1.ToString().ToUpper()[0] == letter
+                select lsr;
+            if(results.Count() > 0)
+            {
+                score = results.First().Item2;
+            }
+            return score;
+        }
+
+        static int GetStringScore(string str)
+        {
+            int score = 0;
+            foreach(var c in str)
+            {
+                score += GetLetterScore(c);
+            }
+            return score;
+        }
+
+        static string DecodeText(string textToDecode)
+        {
+            List<Tuple<string, int>> results = new();
+            foreach(var poss in GenerateAllPossibleOrigText(textToDecode))
+            {
+                results.Add(new(poss, GetStringScore(poss)));
+            }
+            string topResult =
+                (from r in results
+                 orderby r.Item2 descending
+                 select r).First().Item1;
+            return topResult;
+        }
+
+        static void OptionalBonusTwo()
+        {
+            List<string> values = new()
+            {
+                "Zol abyulk tl puav h ulda.",
+                "Tfdv ef wlikyvi, wfi uvrky rnrzkj pfl rcc nzky erjkp, szx, gfzekp kvvky.",
+                "Qv wzlmz bw uiqvbiqv iqz - axmml dmtwkqbg, i aeittwe " +
+                    "vmmla bw jmib qba eqvoa nwzbg - bpzmm bquma mdmzg amkwvl, zqopb?"
+            };
+            values.ForEach(t => Console.WriteLine($" { t } => { DecodeText(t) }"));
+        }
+
         static void Main(string[] args)
         {
             //TestCaeserChar();
             Challenge();
             Console.WriteLine();
             OptionalBonusOne();
+            Console.WriteLine();
+            OptionalBonusTwo();
+            Console.WriteLine();
             Console.ReadLine();
         }
     }
