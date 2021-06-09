4bb96b5ab5b23507fac9f15f314a04dd4bbd503d
sirDudesALot <johnclivejonesuk@hotmail.co.uk>
2021-06-09 12:58:34 +0100

    Temp commit for Challenge 391

--- Challenge391TheABACABASequence/Program.cs
+++ Challenge391TheABACABASequence/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Text;
 
 namespace Challenge391TheABACABASequence
 {
     class Program
     {
+        const string _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        static StringBuilder Aba(int iterations)
+        {
+            return new StringBuilder();
+        }
+
         static void Main(string[] args)
         {
         }
